6d70eed25947b6be995020514714a3ab7247bda9
Aditya Mhatre <140421712+Adiii19@users.noreply.github.com>
2025-03-22 22:42:40 +0530

    Update App.jsx route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,32 @@
-import React from "react";
-import ContextProvider from "./context/Context";
-import MainContent from "./components/MainContent";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Using Router and Routes in v6
+import Auth from "./Auth";
+// Assuming you have a NewScreen component
 
 const App = () => {
+ 
+
   return (
-    <ContextProvider>
-      <div className="flex">
+    <Router>  {/* This is the context provider for React Router hooks */}
+    <Routes>
+      <Route path="/" element={<Auth />} />
+      <Route path="/dashboard" element={
+        <div className="flex animate-fadeIn duration-1000">
         <Sidebar />
+     
+     
         <MainContent />
+    
+        
       </div>
-    </ContextProvider>
+                            
+                                   
+    } />
+      
+    </Routes>
+  </Router>
   );
 };
 
