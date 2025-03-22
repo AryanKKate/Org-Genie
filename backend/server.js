29ec0cf46b857074b91acbc3f05a40f6ee01bfe4
Adiii19 <2022.aditya.mhatre@ves.ac.in>
2025-03-22 18:36:48 +0530

    prompt issue fixed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const queryRoutes = require('./queryRoute');
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 
 const app = express();
 app.use(cors());
@@ -15,6 +17,11 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err));
 
-app.use('/', queryRoutes);
+// Import the functions you need from the SDKs you need
+
+
+app.use('/', (req,res)=>res.send('Welcome to the server'));
+
+
 
 app.listen(3001, () => console.log('Server running on port 3001'));
